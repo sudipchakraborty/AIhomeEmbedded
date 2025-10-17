73decfd9711c16465579964d03d24e1e3112d545
DESKTOP-M80B50R\Dr. S Chakraborty <sudip.embedded@gmail.com>
2025-10-17 17:14:58 +0530

    add files from previous arduino Repo

--- ARDUINO/RELAY.h
+++ ARDUINO/RELAY.h
@@ -1,25 +1,33 @@
-#ifndef RTC_H
-#define RTC_H
-#include <Arduino.h>
+#ifndef RELAY_H
+#define RELAY_H
 
+#include "Arduino.h"
+//////////////////////
 
-class RTC{
+#define   Relay_1     53
+#define   Relay_2     51
+#define   Relay_3     49
+#define   Relay_4     47
+#define   Relay_5     45
+#define   Relay_6     43
+#define   Relay_7     41
+#define   Relay_8     39
 
-
-  private:
-    byte dataPin;
-    byte clockPin;
-    byte latchPin;
-
-  public:
-   
-
-
-
-
-
-
+class Relay
+{
+    public:
+           Relay();
+      void init(void);
+      void update(String str);
+    private:       
 };
+///////////////////////
+
+
+
+
+
+
 
 
 
